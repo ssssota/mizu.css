b584079d904905b4fb10c4016d9dd8093943724e
ssssota <tommysouta@gmail.com>
2024-02-20 23:54:34 +0900

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const minifyMode = mode === "minify";
   return {
     build: {
       rollupOptions: {
-        input: ["src/dark.css", "src/light.css", "src/water.css"],
-        output: { assetFileNames: ({ name }) => name! },
+        input: [
+          "src/dark.css",
+          "src/light.css",
+          "src/mizu.css",
+          "src/core.css",
+        ],
+        output: {
+          assetFileNames: ({ name }) => name!,
+          dir: minifyMode ? "min" : "dist",
+        },
       },
+      cssMinify: minifyMode ? "lightningcss" : false,
     },
   };
 });
