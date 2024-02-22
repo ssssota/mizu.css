7395a9a13e3f23e052bd568b58e34f9e4c6bcd98
ssssota <tommysouta@gmail.com>
2024-02-23 00:56:29 +0900

    chore: setup pages

--- README.md
+++ README.md
@@ -3,3 +3,37 @@
 mizu.css is a classless CSS framework. mizu.css is
 **_extremely_** influenced by water.css, and “mizu”
 comes from the Japanese translation of “water”, 「水」.
+
+## Usage
+
+### CDN
+
+```html
+<!-- auto(light/dark) theme -->
+<link rel="stylesheet" href="https://www.unpkg.com/mizu.css/min/mizu.css" />
+<!-- light theme -->
+<link rel="stylesheet" href="https://www.unpkg.com/mizu.css/min/light.css" />
+<!-- dark theme -->
+<link rel="stylesheet" href="https://www.unpkg.com/mizu.css/min/dark.css" />
+```
+
+### NPM
+
+```sh
+npm install mizu.css
+```
+
+with vite/webpack or other bundlers:
+
+```js
+// auto(light/dark) theme
+import "mizu.css";
+// light theme
+import "mizu.css/light.css";
+// dark theme
+import "mizu.css/dark.css";
+```
+
+## License
+
+MIT
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,28 @@
 import { defineConfig } from "vite";
 
+const assetFileNames = (c: { name?: string }) => c.name!;
+const input = ["src/dark.css", "src/light.css", "src/mizu.css", "src/core.css"];
+
 export default defineConfig(({ mode }) => {
-  const minifyMode = mode === "minify";
-  return {
-    build: {
-      rollupOptions: {
-        input: [
-          "src/dark.css",
-          "src/light.css",
-          "src/mizu.css",
-          "src/core.css",
-        ],
-        output: {
-          assetFileNames: ({ name }) => name!,
-          dir: minifyMode ? "min" : "dist",
+  switch (mode) {
+    case "minify":
+      return {
+        build: {
+          rollupOptions: { input, output: { assetFileNames, dir: "min" } },
+          cssMinify: "lightningcss",
         },
-      },
-      cssMinify: minifyMode ? "lightningcss" : false,
+      };
+    case "normal":
+      return {
+        build: {
+          rollupOptions: { input, output: { assetFileNames, dir: "dist" } },
+        },
+      };
+  }
+  return {
+    base: "./",
+    build: {
+      outDir: "build",
     },
   };
 });
