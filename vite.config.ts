9fb3fbd321db6e089b0f520810b267e8cfdce166
ssssota <tommysouta@gmail.com>
2024-11-06 21:57:32 +0900

    chore: use biome as formatter/linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,33 @@
 import { defineConfig } from "vite";
 
-const assetFileNames = (c: { name?: string }) => c.name!;
+const assetFileNames = (c: { name?: string }) => {
+	if (!c.name) throw new Error("assetFileNames: name is undefined");
+	return c.name;
+};
 const input = ["src/dark.css", "src/light.css", "src/mizu.css", "src/core.css"];
 
 export default defineConfig(({ mode }) => {
-  switch (mode) {
-    case "minify":
-      return {
-        build: {
-          rollupOptions: { input, output: { assetFileNames, dir: "min" } },
-          cssMinify: "lightningcss",
-          copyPublicDir: false,
-        },
-      };
-    case "normal":
-      return {
-        build: {
-          rollupOptions: { input, output: { assetFileNames, dir: "dist" } },
-          copyPublicDir: false,
-        },
-      };
-  }
-  return {
-    base: "./",
-    build: {
-      outDir: "build",
-    },
-  };
+	switch (mode) {
+		case "minify":
+			return {
+				build: {
+					rollupOptions: { input, output: { assetFileNames, dir: "min" } },
+					cssMinify: "lightningcss",
+					copyPublicDir: false,
+				},
+			};
+		case "normal":
+			return {
+				build: {
+					rollupOptions: { input, output: { assetFileNames, dir: "dist" } },
+					copyPublicDir: false,
+				},
+			};
+	}
+	return {
+		base: "./",
+		build: {
+			outDir: "build",
+		},
+	};
 });
